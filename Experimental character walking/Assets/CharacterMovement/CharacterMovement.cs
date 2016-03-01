c84655441e95cb639edf1f760d7453e7f22e96ca
Colin <cbenist@yahoo.com>
2016-03-01 17:47:36 -0500

    includes character packages
    stuff

---
+++ Experimental character walking/Assets/CharacterMovement/CharacterMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+    CharacterController cc;
+    float MoveSpeed = 5.0f;
+    float RotationSpeed = 30.0f;
+    float JumpHeight = 10.0f;
+    bool isJumping = false;
+    Vector3 upward = Vector3.zero;
+    Vector3 lastForward = Vector3.zero;
+     
+
+    public float power = 30.0f;
+    public GameObject weaponPrefab;
+    public GameObject spawnPoint;
+	// Use this for initialization
+	void Start() {
+        cc = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+
+        if (Input.GetButtonDown("Fire1") || Input.GetAxis("Fire1") > 0.8) {
+            weaponThrow(power);
+        }
+
+	}
+
+    
+
+    public Rigidbody weaponThrow(float power) {
+        GameObject g = Instantiate(weaponPrefab, spawnPoint.transform.position, transform.rotation) as GameObject;
+        Rigidbody body = g.GetComponent<Rigidbody>();
+        body.AddForce(spawnPoint.transform.forward.normalized * power, ForceMode.Impulse);
+        return body;
+    }
+
+    void CheckJumping() {
+        if (Input.GetButtonDown("Jump") && cc.isGrounded) {
+            isJumping = true;
+        }
+    }
+    
+
+    
+
+    Vector3 GetUpwardVector(Vector3 forward) {
+        if (!cc.isGrounded)
+        {
+            upward += Physics.gravity * Time.deltaTime;
+        }
+       
+        return upward;
+    }
+
+    void Move() {
+		Vector3 for1 = transform.TransformDirection (Vector3.forward);
+		for1.y = 0.0f;
+        Vector3 forward = Input.GetAxis("LeftVertical") * for1 * MoveSpeed;
+        Vector3 right = Input.GetAxis("LeftHorizontal") * transform.TransformDirection(Vector3.right) * MoveSpeed * 0.85f;
+        transform.Rotate(new Vector3(0, Input.GetAxis("RightHorizontal"), 0) * RotationSpeed * Time.deltaTime);
+		
+
+        CheckJumping();
+        forward += GetUpwardVector(forward);
+        cc.Move(forward * Time.deltaTime);
+        cc.Move(right * Time.deltaTime);
+    }
+
+    
+}
---
+++ Experimental character walking/Assets/CharacterMovement/VerticalCameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalCameraController : MonoBehaviour {
+    float cameraVertRotationSpeed = 5.0f;
+    GameObject GOPlayer;
+    
+
+	// Use this for initialization
+	void Start () {
+        GOPlayer = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vLook;
+        vLook = new Vector3(Input.GetAxis("RightVertical"), 0, 0);
+        transform.Rotate(vLook);
+        
+
+	}
+
+    
+}
