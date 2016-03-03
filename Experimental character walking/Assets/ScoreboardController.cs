8a19b066ba78a18133db973650abbd176b2998a4
empressClay <zhall@ggc.edu>
2016-03-03 17:43:35 -0500

    Trying to code OMG
    Added some code ( ͡° ͜ʖ ͡°) to the credit scene and made a script for
    the GUI/Scoreboard. Not sure how to update it though

---
+++ Experimental character walking/Assets/ScoreboardController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreboardController : MonoBehaviour {
+
+	GameObject Scoreboard;
+	// Use this for initialization
+	void Start () {
+		 = GameObject.Find("Scoreboard");
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
