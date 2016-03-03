aacd67a4c6d35bd20505ff301a156ee2dd5f3627
Alexander Gonzalez <agonzalez2@ggc.edu>
2016-03-03 01:24:37 -0500

    Zombie
    We now have a zombie that follows the player, and respawns more zombies

---
+++ Experimental character walking/Assets/Zombie/ZombieController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+	public GameObject zombie;
+
+	Animator my_anim;
+	NavMeshAgent my_agent;
+	GameObject[] spawnPoints;
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		my_anim = GetComponent<Animator> ();
+		my_agent = GetComponent<NavMeshAgent> ();
+		spawnPoints = GameObject.FindGameObjectsWithTag ("Respawn");
+		target = GameObject.FindGameObjectWithTag ("Player");
+		Invoke ("spawnZombies", Random.Range(3,10));
+	}
+
+	void spawnZombies()
+	{
+		Instantiate (zombie, spawnPoints [Random.Range (0, spawnPoints.Length)].transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		my_agent.destination = target.transform.position;
+		my_anim.SetBool ("isMoving", my_agent.velocity.magnitude > 0);
+	
+	}
+}
