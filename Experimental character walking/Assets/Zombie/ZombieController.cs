e823675fca6068571da472258e67a95cb643bebb
Alexander Gonzalez <agonzalez2@ggc.edu>
2016-03-03 19:45:51 -0500

    Zombies
    Zombies Exist and Die. Have Fun!

---
+++ Experimental character walking/Assets/Zombie/ZombieController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+
+	Animator my_anim;
+	NavMeshAgent my_agent;
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		my_anim = GetComponent<Animator> ();
+		my_agent = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		my_agent.destination = target.transform.position;
+		my_anim.SetBool ("isMoving", my_agent.velocity.magnitude > 0);
+	
+	}
+}
---
+++ Experimental character walking/Assets/ZombieCreator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieCreator : MonoBehaviour {
+
+	public GameObject zombie;
+	GameObject[] spawnPoints;
+
+	int wave;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = GameObject.FindGameObjectsWithTag ("Respawn");
+		wave = 1;
+		spawnZombies ();
+	}
+	
+	void spawnZombies()
+	{
+		for (int i = 0; i < wave; i++) {
+			Instantiate (zombie, spawnPoints [Random.Range (0, spawnPoints.Length)].transform.position, transform.rotation);
+		}
+		wave++;
+		Invoke("spawnZombies", Random.Range(10, 20));
+	}
+}
